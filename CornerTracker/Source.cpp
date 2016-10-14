75790f8585b30f1f97870133d6a027baf8600a28
kurepasu0731 <kurepasu0731@gmail.com>
2016-10-14 18:59:41 +0900

    adaptiveThreshold機能追加

--- CornerTracker/Source.cpp
+++ CornerTracker/Source.cpp
@@ -6,11 +6,13 @@
 
 #include <atltime.h>
 
+#define A_THRESH_VAL -5
+
 int main(int argc, char** argv)
 {
     cv::VideoCapture cap(0);
-	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
-	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
+	cap.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
+	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
 	cap.set(CV_CAP_PROP_FPS, 30);
 
     const int cycle = 30;
@@ -38,8 +40,24 @@ int main(int argc, char** argv)
         cv::Mat currFrameGray;
 
         cv::cvtColor(prevFrame, prevFrameGray, CV_RGB2GRAY);
-        cv::cvtColor(frame, currFrameGray, CV_RGB2GRAY);
+        cv::cvtColor(frame, currFrameGray, CV_BGR2GRAY);
 
+		//適応的閾値処理の確認//
+		cv::Mat adapBinImg, binImg;
+		cv::adaptiveThreshold(currFrameGray, adapBinImg, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY, 7, A_THRESH_VAL);
+		cv::threshold(currFrameGray, binImg, 150, 255, cv::THRESH_BINARY);
+
+		cv::Mat resizeAdap, resizeBin;
+		cv::resize(adapBinImg, resizeAdap, cv::Size(adapBinImg.cols * 0.5, adapBinImg.rows * 0.5));
+		cv::resize(binImg, resizeBin, cv::Size(binImg.cols * 0.5, binImg.rows * 0.5));
+
+		cv::imshow("Threshold", resizeBin);
+		cv::imshow("adaptiveThreshold", resizeAdap);
+		int key = cv::waitKey(cycle);
+		//適応的閾値処理の確認//
+
+
+#if 0
         // 特徴点抽出
         std::vector<cv::Point2f> currCorners; //->whileの外に出してみる
 
@@ -95,6 +113,43 @@ int main(int argc, char** argv)
 		else if(key == 32){
 			refresh = !refresh;
 		}
-    }
+#endif
+
+#if 0
+
+        // 特徴点抽出
+        std::vector<cv::Point2f> prevCorners;
+        std::vector<cv::Point2f> currCorners;
+
+        cv::goodFeaturesToTrack(prevFrameGray, prevCorners, 20, 0.05, 5.0);
+        cv::goodFeaturesToTrack(currFrameGray, currCorners, 20, 0.05, 5.0);
+        cv::cornerSubPix(prevFrameGray, prevCorners, cv::Size(21, 21), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 30, 0.01));
+        cv::cornerSubPix(currFrameGray, currCorners, cv::Size(21, 21), cv::Size(-1, -1), cv::TermCriteria(cv::TermCriteria::COUNT | cv::TermCriteria::EPS, 30, 0.01));
+
+        std::vector<uchar> featuresFound;
+        std::vector<float> featuresErrors;
+
+        cv::calcOpticalFlowPyrLK(
+            prevFrameGray,
+            currFrameGray,
+            prevCorners,
+            currCorners,
+            featuresFound,
+            featuresErrors);
+
+        for (int i = 0; i < featuresFound.size(); i++) {
+            cv::Point p1 = cv::Point((int) prevCorners[i].x, (int) prevCorners[i].y);
+            cv::Point p2 = cv::Point((int) currCorners[i].x, (int) currCorners[i].y);
+
+			//cv::putText(drawframe,std::to_string(i), cv::Point((int) currCorners[i].x, (int) currCorners[i].y), cv::FONT_HERSHEY_SIMPLEX, 0.7, cv::Scalar(255,0,0));
+			//cv::circle(drawframe, p2, 3, cv::Scalar(255,0,0), 2);
+            cv::line(drawframe, p1, p2, cv::Scalar(0, 0, 255), 2);
+        }
+
+        cv::imshow("preview", drawframe);
+        prevFrame = frame;
+        if (cv::waitKey(cycle) == 27) { break; }
+#endif
+	}
     return 0;
 }
